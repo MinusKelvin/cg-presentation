335c8d4b7d41105e66f4014be1e74d703a82a2ca
MinusKelvin <minuskelvin.carlson@gmail.com>
2015-10-03 14:06:14 +1000

    Made it possible to go back on third slide

--- slide2.js
+++ slide2.js
@@ -1,8 +1,8 @@
 var slide2obj = {};
 
 slide2obj.init = function() {
-    slide2obj.next = function() {}
-    slide2obj.prev = function() {}
+    slide2obj.next = function() {return true;}
+    slide2obj.prev = function() {return true;}
 }
 
 slideObjects.push(slide2obj);
